3793cf5059fe8949d5c2f56a351fa3879036ab47
Hazim <hazimabd2@gmail.com>
2023-11-30 13:56:57 +0100

    push

--- Assets/scripts/DroneBrain.cs
+++ Assets/scripts/DroneBrain.cs
@@ -14,7 +14,7 @@ public class DroneBrain : Agent
 
     public Transform[] spawnPoints;
 
-    private float thrust = 0.0368f; // depends on the speed
+    //private float thrust = 0.0368f; // depends on the speed
 
     //private Vector3[] points = {new Vector3()};
     
@@ -43,13 +43,13 @@ public class DroneBrain : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float speed = 8f;
+        float speed = 200f;
         float movex = actions.ContinuousActions[0];
         float movey = actions.ContinuousActions[1] / 2;
         float movez = actions.ContinuousActions[2];
 
         // Counteract gravity by setting the Rigidbody's velocity
-        Vector3 velocity = new Vector3(movex, movey + thrust, movez) * speed;
+        Vector3 velocity = new Vector3(movex, movey , movez) * speed * Time.fixedDeltaTime;
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = velocity;
 
@@ -75,12 +75,12 @@ public class DroneBrain : Agent
         float distance_scaled = Vector3.Distance(DropZone.localPosition, transform.localPosition) / MAX_DISTANCE; // [0, 1] approx
         AddReward(-distance_scaled / 10); // [0, 0.1] negative
 
-
+        /*
         if(Vector3.Distance(transform.localPosition, DropZone.localPosition) < 3f)
         {
             AddReward(1 / (transform.localPosition.y + 1));
         }
-
+        */
         //////math.clampf()
         // could add the conditional reward and leave this as well (no else)
     }
